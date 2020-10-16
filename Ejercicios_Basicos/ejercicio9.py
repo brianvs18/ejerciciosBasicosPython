bb69107194308a5ba2752dff65fc53c8b1256860
Brian Vanegas <brianvs18@gmail.com>
2020-10-15 23:24:04 -0500

    ejercicios completados

--- Ejercicios_Basicos/ejercicio9.py
+++ Ejercicios_Basicos/ejercicio9.py
@@ -1,4 +1,5 @@
-from datetime import date
-hoy = date.today()
-nacimiento = datetime(input("Ingrese la fecha de nacimiento: (AAAA/MM/DD)"))
-print(nacimiento)
+from datetime import  date
+actual = date.today()
+nacimiento = date(1993,5,18)
+diferencia = (actual.year - nacimiento.year) * 12 + actual.month - nacimiento.month
+print(f"La diferencia de tiempo es de",diferencia,"meses hasta hoy")
